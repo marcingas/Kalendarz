d157bd7c269ad68be213a615ddc94fb210dbf0c9
Marcin <gasiorek.mail@gmail.com>
2023-03-03 09:41:26 +0100

    refactor

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int day = 0;
         int month = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("podaj dwie liczby oddzielone przecinkiem(dzień, miesiąc):");
+        System.out.println("podaj dwie liczby oddzielone przecinkiem(dzień,miesiąc):");
         String input = scanner.nextLine();
         String[] table = input.split(",");
 
@@ -21,130 +21,123 @@ public class Main {
         }
         if (month > 12) {
             System.out.println("Podałeś nieprawidłową liczbę miesięcy");
-        } else if (day > 31) {
+            return;
+        }
+        if (day > 31) {
             System.out.println("Podano nieprawidłową liczbę dni ");
-        } else {
-
-
-
-            String d = null;
-            int[]mond = new int[5];
-            int i;
-            mond[0]=1;
-            for(i = 1; i < mond.length; i++){
-                mond[i]=mond[i-1] + 7;
-            }
-            for(i = 0; i < mond.length; i++) {
-                if (day == mond[i]) {
-                    d = "Monday";
-                    break;
-                }
-            }
-
-            mond[0]=2;
-            for(i = 1; i < mond.length; i++){
-                mond[i]=mond[i-1] + 7;
-            }
-            for(i = 0; i < mond.length; i++) {
-                if (day == mond[i]) {
-                    d = "Tuesday";
-                    break;
-                }
-            }
-
-            mond[0]=3;
-            for(i = 1; i < mond.length; i++){
-                mond[i]=mond[i-1] + 7;
-            }
-            for(i = 0; i < mond.length; i++) {
-                if (day == mond[i]) {
-                    d = "Wednesday";
-                    break;
-                }
-            }
-
-            mond[0]=4;
-            for(i = 1; i < mond.length; i++){
-                mond[i]=mond[i-1] + 7;
-            }
-            for(i = 0; i < mond.length; i++) {
-                if (day == mond[i]) {
-                    d = "Thursday";
-                    break;
-                }
-            }
-
-            mond[0]=5;
-            for(i = 1; i < mond.length; i++){
-                mond[i]=mond[i-1] + 7;
-            }
-            for(i = 0; i < mond.length; i++) {
-                if (day == mond[i]) {
-                    d = "Friday";
-                    break;
-                }
-            }
-            mond[0]=6;
-            for(i = 1; i < mond.length; i++){
-                mond[i]=mond[i-1] + 7;
-            }
-            for(i = 0; i < mond.length; i++) {
-                if (day == mond[i]) {
-                    d = "Saturday";
-                    break;
-                }
-            }
-
-            mond[0]=7;
-            for(i = 1; i < mond.length; i++){
-                mond[i]=mond[i-1] + 7;
-            }
-            for(i = 0; i < mond.length; i++) {
-                if (day == mond[i]) {
-                    d = "Sunday";
-                    break;
-                }
-            }
-
-
-            String m;
-            if(month == 1){
-                m="Styczeń";
-            }else if(month == 2){
-                m = "Luty";
-            }else if(month == 3){
-                m = "Marzec";
-            }else if (month == 4){
-                m="Kwiecień";
-            }else if(month == 5){
-                m= "Maj";
-            }else if(month == 6){
-                m = "Czerwiec";
-            }else if(month == 7){
-                m = "Lipiec";
-            }else if(month == 8){
-                m = "Sierpień";
-            }else if(month == 9){
-                m = "Wrzesień";
-            }else if(month == 10){
-                m = "Październik";
-            }else if(month == 11){
-                m = "Listopad";
-            }else if(month == 12){
-                m = "Grudzień";
-            }else{
-                m= null;
-            }
-
-            if ((month == 2 && day > 28) || (month==4 && day> 30) || (month == 6 && day > 30) || (month == 9 && day > 30) || (month == 11 && day > 30)  ) {
-                System.out.println("podano za dużo dni!");
-            } else {
-                System.out.println("W miesiącu: " + m + " dzień nr:" + day + " to: " + d);
+            return;
+        }
+        String d = null;
+        int[] dayOfWeek = new int[5];
+        int i;
+        dayOfWeek[0] = 1;
+        for (i = 1; i < dayOfWeek.length; i++) {
+            dayOfWeek[i] = dayOfWeek[i - 1] + 7;
+        }
+        for (i = 0; i < dayOfWeek.length; i++) {
+            if (day == dayOfWeek[i]) {
+                d = "Monday";
+                break;
             }
         }
 
+        dayOfWeek[0] = 2;
+        for (i = 1; i < dayOfWeek.length; i++) {
+            dayOfWeek[i] = dayOfWeek[i - 1] + 7;
+        }
+        for (i = 0; i < dayOfWeek.length; i++) {
+            if (day == dayOfWeek[i]) {
+                d = "Tuesday";
+                break;
+            }
+        }
 
+        dayOfWeek[0] = 3;
+        for (i = 1; i < dayOfWeek.length; i++) {
+            dayOfWeek[i] = dayOfWeek[i - 1] + 7;
+        }
+        for (i = 0; i < dayOfWeek.length; i++) {
+            if (day == dayOfWeek[i]) {
+                d = "Wednesday";
+                break;
+            }
+        }
+        dayOfWeek[0] = 4;
+        for (i = 1; i < dayOfWeek.length; i++) {
+            dayOfWeek[i] = dayOfWeek[i - 1] + 7;
+        }
+        for (i = 0; i < dayOfWeek.length; i++) {
+            if (day == dayOfWeek[i]) {
+                d = "Thursday";
+                break;
+            }
+        }
+        dayOfWeek[0] = 5;
+        for (i = 1; i < dayOfWeek.length; i++) {
+            dayOfWeek[i] = dayOfWeek[i - 1] + 7;
+        }
+        for (i = 0; i < dayOfWeek.length; i++) {
+            if (day == dayOfWeek[i]) {
+                d = "Friday";
+                break;
+            }
+        }
+        dayOfWeek[0] = 6;
+        for (i = 1; i < dayOfWeek.length; i++) {
+            dayOfWeek[i] = dayOfWeek[i - 1] + 7;
+        }
+        for (i = 0; i < dayOfWeek.length; i++) {
+            if (day == dayOfWeek[i]) {
+                d = "Saturday";
+                break;
+            }
+        }
+        dayOfWeek[0] = 7;
+        for (i = 1; i < dayOfWeek.length; i++) {
+            dayOfWeek[i] = dayOfWeek[i - 1] + 7;
+        }
+        for (i = 0; i < dayOfWeek.length; i++) {
+            if (day == dayOfWeek[i]) {
+                d = "Sunday";
+                break;
+            }
+        }
+        String m;
+        if (month == 1) {
+            m = "Styczeń";
+        } else if (month == 2) {
+            m = "Luty";
+        } else if (month == 3) {
+            m = "Marzec";
+        } else if (month == 4) {
+            m = "Kwiecień";
+        } else if (month == 5) {
+            m = "Maj";
+        } else if (month == 6) {
+            m = "Czerwiec";
+        } else if (month == 7) {
+            m = "Lipiec";
+        } else if (month == 8) {
+            m = "Sierpień";
+        } else if (month == 9) {
+            m = "Wrzesień";
+        } else if (month == 10) {
+            m = "Październik";
+        } else if (month == 11) {
+            m = "Listopad";
+        } else if (month == 12) {
+            m = "Grudzień";
+        } else {
+            m = null;
+        }
+
+        if ((month == 2 && day > 28) || (month == 4 && day > 30) || (month == 6 && day > 30) || (month == 9 && day > 30) || (month == 11 && day > 30)) {
+            System.out.println("podano za dużo dni!");
+        } else {
+            System.out.println("W miesiącu: " + m + " dzień nr:" + day + " to: " + d);
+        }
     }
-
-
 }
+
+
+
